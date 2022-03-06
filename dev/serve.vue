e6c7ad43c680b9e28f7eaa642c01d603cc4ad862
MohsenVaziri <mohsen96vaziri@gmail.com>
2022-03-06 16:54:25 +0100

    init

---
+++ dev/serve.vue
@@ -0,0 +1,17 @@
+<script lang="ts">
+import { defineComponent } from 'vue';
+import Vue3HamburgerButton from '@/vue3-hamburger-button.vue';
+
+export default defineComponent({
+  name: 'ServeDev',
+  components: {
+    Vue3HamburgerButton
+  }
+});
+</script>
+
+<template>
+  <div id="app">
+    <vue3-hamburger-button />
+  </div>
+</template>
